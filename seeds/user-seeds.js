2fd2f80f2ef42098059d62025f6a7ed12f03d723
FigWheyMutton <alexhsieh12@gmail.com>
2022-12-14 19:24:28 -0800

    push ok copy

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,13 +2,6 @@ const { User } = require('../models');
 const helpers = require('../utils/helpers');
 
 const userData = [
-    {
-        seeds: 1,
-        username: 'FigWheyMutton',
-        email: 'alexhsieh12@gmail.com',
-        password: 'password',
-        bio:'I am helpers',
-    }
 ]
 
 const seedUsers = () => User.bulkCreate(userData);
